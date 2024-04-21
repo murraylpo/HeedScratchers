d0ae18195c1e8355125ed7c5a9ca2430c41f40d5
Connor Murray <murraylpo@gmail.com>
2024-04-21 13:36:18 +0100

    Full code push as a test. Many tweaks still required before fully functional

---
+++ js/player.js
@@ -0,0 +1,61 @@
+function populateStorage() {
+    let person = prompt("Please enter your name");
+    if (person != null) {
+        localStorage.setItem("name", person);
+        localStorage.setItem("points","0");
+        location.reload();
+    }
+}
+
+function addPoints(amount) {
+    let points = Number(localStorage.getItem("points"));
+    points += amount
+    localStorage.setItem("points", points);
+    sleep(300);
+}
+
+function resetPoints() {
+    localStorage.setItem("points","0");
+}
+
+function removeProfile() {
+    localStorage.clear()
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+if (!localStorage.getItem("name")) {
+    populateStorage();
+}
+
+// Boostrap code for creating dismissable success alerts
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+
+const alert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message} <a href="/index.html" class="alert-link">Return home for more puzzles.</a>
+    </div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    alert('You solved the puzzle and earned points!', 'success')
+  })
+}
+
+// Bootstrap code for carousel
+const myCarouselElement = document.querySelector('#myCarousel')
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 2000,
+  wrap: false
+})
---
+++ puzzles/quiz/quiz.js
@@ -0,0 +1,26 @@
+var myQuestions = [
+	{
+		question: "What is 10/2?",
+		answers: {
+			a: '3',
+			b: '5',
+			c: '115'
+		},
+		correctAnswer: 'b'
+	},
+	{
+		question: "What is 30/3?",
+		answers: {
+			a: '3',
+			b: '5',
+			c: '10'
+		},
+		correctAnswer: 'c'
+	}
+];
+
+function createQuiz() {
+    // Store HTML output
+    let output = [];
+
+}
